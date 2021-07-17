346e474b762b99778b24c108682c1d1146f19481
Facundo Brum <facundodavidbrum@gmail.com>
2021-07-17 04:42:40 +0100

    probando

--- components/Work.vue
+++ components/Work.vue
@@ -2,7 +2,7 @@
     <div class="bg-white h-min flex text-left justify-between rounded-xl shadow-xl w-100 p-5">
 
         <div class="w-84 relative">
-            <h2 class="font-bold text-2xl mb-3">{{ title }}</h2>
+            <h2 class="font-bold text-2xl mb-3 w-80">{{ title }}</h2>
             <p class="w-72">{{description}}</p>
             <div class="absolute bottom-0 left-0">
                 <p class="uppercase w-min rounded-xl px-2 text-sm border-blue-800 font-semibold text-blue-800 bg-blue-50 border-2">{{ tag }}</p>
