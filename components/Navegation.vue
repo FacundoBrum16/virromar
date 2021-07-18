479d8a7171acb88e0a398149e9e34374e61deee6
Facundo Brum <facundodavidbrum@gmail.com>
2021-07-18 17:25:46 +0100

    Arreglue el a href en los botones..

--- components/Navegation.vue
+++ components/Navegation.vue
@@ -7,15 +7,12 @@
 
         <div class="flex w-9/12 items-center justify-end">          
         <div class="flex justify-end">
-            <div class="bg-blue-800 hover:bg-blue-600 cursor-pointer shadow-xl p-1 w-40 h-10 flex justify-center items-center text-right rounded-md">
-                <a class="flex" href="tel:094840062">
-                    <svg width="20" height="20" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M21.0668 15.8375C19.524 15.8375 18.0307 15.5896 16.6428 15.1311C16.2153 14.9948 15.7258 15.094 15.385 15.4348L12.6587 18.1672C9.14553 16.3828 6.27671 13.5139 4.49223 10.007L7.2185 7.26825C7.55931 6.92745 7.65841 6.43799 7.52213 6.01046C7.06364 4.62255 6.81577 3.12926 6.81577 1.58643C6.81571 0.898603 6.26428 0.347168 5.57651 0.347168H1.2392C0.557651 0.347168 0 0.898603 0 1.58637C0 13.2227 9.43051 22.6532 21.0668 22.6532C21.7546 22.6532 22.306 22.1018 22.306 21.414V17.0767C22.306 16.3889 21.7546 15.8375 21.0668 15.8375Z" fill="white"/>
-                    </svg>
-                    <button class="font-bold text-md text-white uppercase ml-3">Consulta</button>
-                </a>
-
-            </div>
+            <a href="tel:094840062" class="bg-blue-800 hover:bg-blue-600 cursor-pointer shadow-xl p-1 w-40 h-10 flex justify-center items-center text-right rounded-md">
+                <svg width="20" height="20" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21.0668 15.8375C19.524 15.8375 18.0307 15.5896 16.6428 15.1311C16.2153 14.9948 15.7258 15.094 15.385 15.4348L12.6587 18.1672C9.14553 16.3828 6.27671 13.5139 4.49223 10.007L7.2185 7.26825C7.55931 6.92745 7.65841 6.43799 7.52213 6.01046C7.06364 4.62255 6.81577 3.12926 6.81577 1.58643C6.81571 0.898603 6.26428 0.347168 5.57651 0.347168H1.2392C0.557651 0.347168 0 0.898603 0 1.58637C0 13.2227 9.43051 22.6532 21.0668 22.6532C21.7546 22.6532 22.306 22.1018 22.306 21.414V17.0767C22.306 16.3889 21.7546 15.8375 21.0668 15.8375Z" fill="white"/>
+                </svg>
+                <button class="font-bold text-md text-white uppercase ml-3">Consulta</button>
+            </a>
         </div>
         </div>
     </div>
